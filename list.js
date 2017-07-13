a5805e5d1f372bb3a83facd8062dcf48b04118b0
InnovationBham_1 <InnovationBham_1@UABiLab-1s-iMac.local>
2017-07-13 16:20:08 -0500

    final commit

--- list.js
+++ list.js
@@ -5,15 +5,18 @@ $(document).ready(function(){
     })
     .then(function(success){
         console.log(success)
-    for (var i=1; i < 5; i++){
+    for (var i=0; i < 10; i++){
     var MYdata = success.data.children[i].data.title
-    var Titlediv = document.createElement('h2')
-    document.body.append(Titlediv)
-    Titlediv.append(MYdata)
+    var Title = document.createElement('h2')
+    document.body.append(Title)
     var MYposts = success.data.children[i].data.selftext
     var Postdiv = document.createElement('div')
     document.body.append(Postdiv)
     Postdiv.append(MYposts)
+    var a = document.createElement('a')
+    a.text = success.data.children[i].data.title
+    a.href = 'single.html?url=' + success.data.children[i].data.permalink
+    Title.append(a)
     }
     })
 });
--- single.js
+++ single.js
@@ -1,10 +1,23 @@
+var search = location.search.substring(1);
+var query = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
+
 $(document).ready(function(){
     $.ajax({
     method: "GET",
-    url:'https://www.reddit.com/r/darksouls3/.json'
+    url:'https://www.reddit.com' + query.url + '.json'
     })
     .then(function(success){
         console.log(success)
-        
+        var Title = document.createElement('h2')
+        var Postdiv = document.createElement('div')
+        var Image = document.createElement('div')
+        Image.src = success[0].data.children[0].data.thumbnail;
+        var innerposts = success[0].data.children[0].data.selftext
+        var innerdata = success[0].data.children[0].data.title
+        document.body.append(Title)
+        document.body.append(Postdiv)
+        Postdiv.append(innerposts)
+        Title.append(innerdata)
+
     })
 })
